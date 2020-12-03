63f45cdf8b34ccd5a7fe05da6e762331ced1224d
GourabRoy551 <gourabroysec553@gmail.com>
2020-12-04 02:15:03 +0600

    PassStatement

---
+++ PassStatement.py
@@ -0,0 +1,12 @@
+"""pass is just a placeholder for
+functionality to be added later."""
+sequence = {'p', 'a', 's', 's'}
+for val in sequence:
+    pass
+
+# The same thing in an empty function or class as well.
+    def function(args):
+        pass
+
+    class Example:
+        pass
